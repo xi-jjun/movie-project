679d465aae676b36164e11b0bec9b810662f5989
xi_jjun <rlawowns97@naver.com>
2022-05-22 14:17:17 +0900

    refactor: Member entity 수정, 생성자 추가 및 update method 추가
    getRoleList method
    security에서 인증을 진행할 때 해당 Member에 대한 role정보를 얻기위해 작성.
    
    rank field 추가. 영화 덕후력 테스트 결과로서 총 5단계가 있음.

--- community/src/main/java/com/movie/community/domain/Member.java
+++ community/src/main/java/com/movie/community/domain/Member.java
@@ -1,12 +1,23 @@
 package com.movie.community.domain;
 
+import com.movie.community.controller.dto.request.MemberRequestDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Member {
 	@Id
@@ -27,5 +38,56 @@ public class Member {
 	private int age;
 
 	@Column
+	@Enumerated(EnumType.STRING)
+	private Rank rank;
+
+	@Column
+	private String roles;
+//	private Role role;
+
+	@Column
 	private LocalDateTime createdDate;
+
+	public Member(MemberRequestDTO requestDTO) {
+		insertDtoDataToEntity(requestDTO);
+		this.roles = requestDTO.getRoles();
+		this.createdDate = LocalDateTime.now();
+	}
+
+	public void update(MemberRequestDTO requestDTO) {
+		insertDtoDataToEntity(requestDTO);
+	}
+
+	public void setRank(String rank) {
+		for (Rank value : Rank.values()) {
+			if (value.isEqual(rank)) {
+				this.rank = value;
+				return;
+			}
+		}
+	}
+
+	public List<String> getRoleList() {
+		if (this.roles.length() > 0) {
+			return Arrays.asList(this.roles.split(","));
+		} else {
+			return new ArrayList<>();
+		}
+	}
+//	public void setRole(String role) {
+//		for (Role value : Role.values()) {
+//			if (value.isEqual(role)) {
+//				this.role = value;
+//				return;
+//			}
+//		}
+//	}
+
+	private void insertDtoDataToEntity(MemberRequestDTO requestDTO) {
+		this.name = requestDTO.getName();
+		this.account = requestDTO.getAccount();
+		this.password = requestDTO.getPassword();
+		this.age = requestDTO.getAge();
+		setRank(requestDTO.getRank());
+	}
 }
