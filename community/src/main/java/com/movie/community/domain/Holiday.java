21e0f86d992814f6ca30da0a151498df7f1a2c28
xi_jjun <rlawowns97@naver.com>
2022-05-22 13:05:27 +0900

    refactor: modify Holiday entity for making constructor and update method
    @Getter 추가.
    
    Line:29
    LocalDateTime 에서 LocalDate로 data를 바꿈. 기념일이라는 날짜는 시간, 분, 초의 정보는 담을 필요가 없다고 판단함.
    
    Line:31-32
    Holiday api에서는 '어떤 기념일에 해당되는 모든 영화 목록을 조회' 할 수 있는 기능을 제공해야 한다. 따라서 DB에서 바로
    query를 생성하여 가져와도 좋지만, 역참조를 통해 가져오는게 더 편리해 보였기에 해당 @OneToMany를 사용.
    
    Holiday 생성자 : 기념일을 추가하기 위해서 DTO로 받아온 기념일 정보를 entity로 바꿀 때 해당 생성자를 통해서 바꿀 수 있다.
    
    update method : 기념일에 대한 정보를 수정하기 위해서 DTO로 받아온 정보로 업데이트 할 수 있게 하기 위함이다.

--- community/src/main/java/com/movie/community/domain/Holiday.java
+++ community/src/main/java/com/movie/community/domain/Holiday.java
@@ -1,12 +1,20 @@
 package com.movie.community.domain;
 
+import com.movie.community.controller.dto.request.HolidayRequestDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDateTime;
+import javax.persistence.OneToMany;
+import java.time.LocalDate;
+import java.util.List;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Holiday {
 	@Id
@@ -18,5 +26,18 @@ public class Holiday {
 	private String holiday;
 
 	@Column
-	private LocalDateTime date;
+	private LocalDate date;
+
+	@OneToMany(mappedBy = "holiday")
+	private List<Movie> movies;
+
+	public Holiday(HolidayRequestDTO requestDTO) {
+		this.holiday = requestDTO.getHoliday();
+		this.date = requestDTO.getDate();
+	}
+
+	public void update(HolidayRequestDTO requestDTO) {
+		this.holiday = requestDTO.getHoliday();
+		this.date = requestDTO.getDate();
+	}
 }
