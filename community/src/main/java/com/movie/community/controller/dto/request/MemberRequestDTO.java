86ef260381996afc58f34a6c1815fca64b005b04
xi_jjun <rlawowns97@naver.com>
2022-05-22 13:48:24 +0900

    feat: add MemberDTO
    MemberRequestDTO
    roles는 권한정보를 문자열로 받겠다는 뜻이다. 따라서
    ex) input : 'admin, user'
    output : user authority : admin, user 가 된다는 것.
    
    rank : 영화 덕후력 테스트를 진행하면 받게 되는 랭크이다.

---
+++ community/src/main/java/com/movie/community/controller/dto/request/MemberRequestDTO.java
@@ -0,0 +1,18 @@
+package com.movie.community.controller.dto.request;
+
+import com.movie.community.domain.Member;
+import lombok.Data;
+
+@Data
+public class MemberRequestDTO {
+	private String name;
+	private String account;
+	private String password;
+	private int age;
+	private String rank;
+	private String roles;
+
+	public Member toEntity() {
+		return new Member(this);
+	}
+}
---
+++ community/src/main/java/com/movie/community/controller/dto/response/MemberResponseDTO.java
@@ -0,0 +1,25 @@
+package com.movie.community.controller.dto.response;
+
+import com.movie.community.domain.Member;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+public class MemberResponseDTO {
+	private String name;
+	private String account;
+	private int age;
+	private String rank;
+	private String roles;
+	private LocalDateTime createdDate;
+
+	public MemberResponseDTO(Member member) {
+		this.name = member.getName();
+		this.account = member.getAccount();
+		this.age = member.getAge();
+		this.rank = member.getRank().getValue();
+		this.roles = member.getRoles();
+		this.createdDate = member.getCreatedDate();
+	}
+}
