331f6ce1edd29dffdf0a80d3ab304ad5e47e1acc
xi_jjun <rlawowns97@naver.com>
2022-05-26 13:51:40 +0900

    refactor: Comment 수정 for DTO,Entity만들기 위한 생성자 추가 및 연관관계 mapping method 추가

--- community/src/main/java/com/movie/community/domain/Comment.java
+++ community/src/main/java/com/movie/community/domain/Comment.java
@@ -1,6 +1,9 @@
 package com.movie.community.domain;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.movie.community.controller.dto.request.CommentRequestDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
@@ -12,6 +15,8 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Comment {
 	@Id
@@ -37,4 +42,22 @@ public class Comment {
 	@ManyToOne(targetEntity = Posting.class, fetch = FetchType.LAZY)
 	@JoinColumn(name = "posting_id")
 	private Posting posting;
+
+	public Comment(CommentRequestDTO requestDTO) {
+		this.comment = requestDTO.getComment();
+		this.createdDate = LocalDateTime.now();
+	}
+
+	public void update(CommentRequestDTO requestDTO) {
+		this.comment = requestDTO.getComment();
+		this.updatedDate = LocalDateTime.now();
+	}
+
+	public void setWriter(Member member) {
+		this.member = member;
+	}
+
+	public void mapPosting(Posting posting) {
+		this.posting = posting;
+	}
 }
