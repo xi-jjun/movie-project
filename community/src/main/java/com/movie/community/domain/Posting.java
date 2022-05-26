f47fd96efc255b80273fcc8d3390c395b5e1c4b7
xi_jjun <rlawowns97@naver.com>
2022-05-26 13:37:23 +0900

    refactor: Posting 수정 for 연관관계를 위한 method정의, DTO와 Entity생성을 위한 생성자 정의

--- community/src/main/java/com/movie/community/domain/Posting.java
+++ community/src/main/java/com/movie/community/domain/Posting.java
@@ -1,5 +1,10 @@
 package com.movie.community.domain;
 
+import com.movie.community.controller.dto.request.PostingRequestDTO;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
@@ -12,12 +17,17 @@ import javax.persistence.OneToMany;
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Getter
+@NoArgsConstructor
 @Entity
 public class Posting {
 	@Id
 	@Column(name = "posting_id")
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
+	@Column
+	private String title;
 
 	@Column(columnDefinition = "TEXT")
 	private String content;
@@ -32,6 +42,25 @@ public class Posting {
 	@JoinColumn(name = "member_id")
 	private Member member;
 
-	@OneToMany(mappedBy = "posting")
+	@OneToMany(mappedBy = "posting", cascade = CascadeType.REMOVE)
 	private List<Comment> comments;
+
+	public Posting(PostingRequestDTO requestDTO) {
+		insertDtoToEntity(requestDTO);
+		this.createdDate = LocalDateTime.now();
+	}
+
+	public void update(PostingRequestDTO requestDTO) {
+		insertDtoToEntity(requestDTO);
+		this.updatedDate = LocalDateTime.now();
+	}
+
+	public void setWriter(Member member) {
+		this.member = member;
+	}
+
+	private void insertDtoToEntity(PostingRequestDTO requestDTO) {
+		this.title = requestDTO.getTitle();
+		this.content = requestDTO.getContent();
+	}
 }
